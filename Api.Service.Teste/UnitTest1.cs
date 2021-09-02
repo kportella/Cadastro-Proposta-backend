c22ccf87365986b7e114151a5d4065c27ce53e35
Kaue Portella <portellakaue@gmail.com>
2021-09-02 14:23:29 -0300

    Adicionando mais um InlineData

--- Api.Service.Teste/UnitTest1.cs
+++ Api.Service.Teste/UnitTest1.cs
@@ -9,6 +9,7 @@ namespace serviceteste
     {
         [Theory]
         [InlineData(10, 1000, 1.01, 1104.62)]
+        [InlineData(20, 1500, 1.01, 1830.29)]
         public void Test1(int prazo, double valorSolicitado, double taxaJuro, double valorTeste)
         {
             double teste = CalcularValorSolicitadoService.CalcularValorSolicitado(new CalcularValorDto { Prazo = prazo, Vlr_Solicitado = valorSolicitado }, taxaJuro);
